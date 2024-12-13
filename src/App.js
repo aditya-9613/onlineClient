9561f0ef5176df149f259b3ea15984f890938c1c
aditya-9613 <155987958+aditya-9613@users.noreply.github.com>
2024-12-13 17:42:28 +0000

    api call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [user,setUser]=useState('');
+  const apiCheck=()=>{
+    axios({
+      method:'POST',
+      url:'http://localhost:8080',
+      data:({user})
+    }).then((res)=>{
+      console.log(res)      
+    }).catch((err)=>{
+      console.log(err)      
+    })
+  }
   return (
     <div className="App">
-      
+      <h1>Hello World</h1>
+      <input onChange={((e)=>setUser(e.target.value))} type='text' />
+      <button onClick={apiCheck}>Click Me!</button>      
     </div>
   );
 }
